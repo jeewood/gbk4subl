5d5fd92463a2b56fc99aeae4513db0defc9bf29d
jeewood <jeewood@126.com>
2013-06-01 20:16:36 +0800

    gbk encoding check function improve version

--- gbk.py
+++ gbk.py
@@ -19,22 +19,32 @@ def isView(id):
 
 def file_encoding(view):
     try:
-        if os.path.exists(view.file_name()):
-            try:
-                fp = open(view.file_name(),'rb')
-                buf = fp.read(4096)
-                fp.close()
-                buf = buf.decode('gbk')
+        os.path.exists(view.file_name())
+    except:
+        return 'NODISKFILE'
+
+    fp = open(view.file_name(),'rb')
+    buf = fp.read(4096)
+    fp.close()
+    try:
+        buf.decode('utf8')
+        log('utf8 ok')
+        return 'UTF-8'
+    except UnicodeDecodeError as e:
+        try:
+            buf.decode('gbk')
+            view.set_status('_ISGBKFILE','GBK')
+            view.set_encoding('UTF-8')
+            log('gbk ok')
+            return 'GBK'
+        except UnicodeDecodeError as e:
+            if str.find(str(e),'4095')!=-1:
                 view.set_status('_ISGBKFILE','GBK')
                 view.set_encoding('UTF-8')
+                log('gbk ok')
                 return 'GBK'
-            except UnicodeDecodeError as e:
-                if str.find(str(e),'4095')!=-1:
-                    view.set_status('_ISGBKFILE','GBK')
-                    view.set_encoding('UTF-8')
-                    return 'GBK'
-    except:
-        pass 
+            else:
+                log('encoding check err')
     return 'UNKNOWN'
 
 class FromUtf8Command(sublime_plugin.TextCommand):
@@ -56,6 +66,7 @@ class ToUtf8Command(sublime_plugin.TextCommand):
         view = self.view
         if isView(view.id()):
             if view.file_name() and os.path.exists(view.file_name()):
+                #sublime.status_message('GBK -> UTF-8')
                 fp = open(view.file_name(),'rb')
                 buf = fp.read()
                 fp.close()
@@ -96,10 +107,16 @@ class PluginEventListener(sublime_plugin.EventListener):
                     view.run_command('to_utf8')
                     #view.set_status(str(view.file_name()) + '_GBK_STATUS','<A>')
 
+    def on_text_command(self, view, command_name, args):
+        if isView(view.id()):
+            if view.get_status('_ISGBKFILE')=='' or view.encoding() != 'UTF-8':
+                if file_encoding(view)=='GBK':
+                    log('on_activated than to UTF-8')
+                    view.run_command('to_utf8')
+
     def on_modified_async(self, view):
         if isView(view.id()):
             if view.file_name() and os.path.exists(view.file_name()):
-                log('on_modified:  ' + view.get_status(str(view.file_name()) + '_GBK_STATUS'))
                 if view.encoding()!='UTF-8' and file_encoding(view)=='GBK':
                     log('on_modified than to UTF-8')
                     view.run_command('to_utf8')
