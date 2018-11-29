03a858dacb4db6345d296cdd5b732615ee096db9
jeewood <jeewood@126.com>
2018-11-29 15:48:57 +0800

    Update gbk.py
    optimize

--- gbk.py
+++ gbk.py
@@ -1,6 +1,8 @@
 import sublime, sublime_plugin
 import os
 
+cnt = 3
+
 def log(msg):
     print(msg)
     pass
@@ -30,21 +32,20 @@ def DelVar(var):
     view = GetView()
     if(isView(view.id())):
         view.erase_status(var)
-            
-def restore_position_cb():
+
+def done():
     view = GetView()
-    lptx = float(GetVar('_pos_layout_x'))
-    lpty = float(GetVar('_pos_layout_y'))
-    #print('point:',lptx,lpty)
-    posa =  int(GetVar('_pos_x'))
-    posb =  int(GetVar('_pos_y'))
-    pos = sublime.Region(posa,posb)
-    view.sel().add(pos)
-    view.sel().subtract(view.sel()[0])
-    view.set_viewport_position((lptx+1,lpty))
-    view.set_viewport_position((lptx,lpty))
-    DelVar('_pos_layout_x')
-    DelVar('_pos_layout_y')
+    sets=sublime.load_settings(view.file_name())
+    lptx = float(sets.get('_pos_layout_x'))
+    lpty = float(sets.get('_pos_layout_y'))
+    view.set_viewport_position((lptx,lpty),False)
+    sets.erase('_pos_layout_x')
+    sets.erase('_pos_layout_y')
+    if sets.has('_pos_x'):
+        pos = int(sets.get('_pos_x'))
+        view.sel().clear()
+        view.sel().add(sublime.Region(pos,pos))
+        sets.erase('_pos_x')
 
 def file_encoding(view):
     try:
@@ -52,7 +53,7 @@ def file_encoding(view):
     except:
         return 'NODISKFILE'
     fp = open(view.file_name(),'rb')
-    buf = fp.read(4096)
+    buf = fp.read(40960)
     fp.close()
     try:
         buf.decode('utf8')
@@ -64,7 +65,7 @@ def file_encoding(view):
             view.set_encoding('UTF-8')
             return 'GBK'
         except UnicodeDecodeError as e:
-            if str.find(str(e),'4095')!=-1:
+            if str.find(str(e),'40959')!=-1:
                 SetVar('_ISGBKFILE','GBK')
                 view.set_encoding('UTF-8')
                 return 'GBK'
@@ -74,6 +75,7 @@ class FromUtf8Command(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
         if isView(view.id()):
+            view.set_scratch(True)
             reg_all = sublime.Region(0, view.size())
             text = view.substr(reg_all)
             text = text.replace('\n','\r\n')
@@ -82,50 +84,44 @@ class FromUtf8Command(sublime_plugin.TextCommand):
                 fp = open(view.file_name(),'wb')
                 fp.write(text)
                 fp.close()
-                view.set_scratch(True)
+                #view.set_scratch(True)
 
 class ToUtf8Command(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
         if isView(view.id()) and view.file_name() and os.path.exists(view.file_name()):
+            global cnt
+            cnt = 0
+            view.set_scratch(True)
             fp = open(view.file_name(),'rb')
             buf = fp.read()
             fp.close()
             text = buf.decode('gbk')
             text = text.replace('\r\n','\n').replace('\r','\n')
             reg_all = sublime.Region(0, view.size())
-            SetVar('__GBK_S','<L>')
             view.replace(edit,reg_all,text)
-            view.set_scratch(True)
+
+            sets=sublime.load_settings(view.file_name())
+            
+            if sets.has('_pos_layout_x') and sets.has('_pos_layout_y'):
+                sublime.set_timeout(done,1)
 
 class SaveUtf8Command(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
         if isView(view.id()):
-            #DelVar('_ISGBKFILE')
-            DelVar('__GBK_S')
             view.run_command('save',{"encoding": "utf-8"})
 
 class PluginEventListener(sublime_plugin.EventListener):
     def on_load(self, view):
         if isView(view.id()) and  GetVar('_ISGBKFILE')!='GBK' and file_encoding(view)=='GBK':
+            view.set_scratch(True)
             view.run_command('to_utf8')
-
+    
     def on_post_save(self, view):
         if isView(view.id()) and GetVar('_ISGBKFILE')=='GBK':
             view.run_command('from_utf8')
-
-    def on_pre_save(self, view):
-        if isView(view.id()):
-            file_encoding(view)
-            pos = view.sel()[0]
-            lpt = view.viewport_position()
-            print('vp:',lpt,'Sel:',pos)
-            SetVar('_pos_x',str(pos.a))
-            SetVar('_pos_y',str(pos.b))
-            SetVar('_pos_layout_x',str(lpt[0]))
-            SetVar('_pos_layout_y',str(lpt[1]))
-
+    
     def on_close(self, view):
         if isView(view.id()) and GetVar('_ISGBKFILE')=='GBK':
             view.run_command('from_utf8')
@@ -134,27 +130,26 @@ class PluginEventListener(sublime_plugin.EventListener):
         if isView(view.id()) and (GetVar('_ISGBKFILE')=='' or view.encoding() != 'UTF-8') \
             and file_encoding(view)=='GBK':
             view.run_command('to_utf8')
-
+    
     def on_text_command(self, view, command_name, args):
         if isView(view.id()) and (GetVar('_ISGBKFILE')=='' or view.encoding() != 'UTF-8') \
             and file_encoding(view)=='GBK':
             view.run_command('to_utf8')
     
+    def on_pre_save(self,view): #on_selection_modified
+        sets=sublime.load_settings(view.file_name())
+        pos = view.sel()[0].begin()
+        lpt = view.viewport_position()
+        sets.set('_pos_layout_x',str(lpt[0]))
+        sets.set('_pos_layout_y',str(lpt[1]))
+        sets.set('_pos_x',str(pos))
 
-    def on_modified(self, view):
-        if isView(view.id()):
-            if view.file_name() and os.path.exists(view.file_name()):
-                if view.encoding()!='UTF-8' and file_encoding(view)=='GBK':
-                    view.run_command('to_utf8')
-                elif GetVar('__GBK_S')=='<L>':
-                    SetVar('__GBK_S','<A>')
-                elif GetVar('__GBK_S')=='<A>':
-                    DelVar('__GBK_S')
-                    view.set_scratch(True)
-                    sublime.set_timeout(restore_position_cb,50)
-                else:
-                    view.set_scratch(False)
-            if view.is_dirty():
-                DelVar('__GBK_S')
-
-
+    def on_modified(self,view):
+        global cnt
+        if cnt==1 :
+            view.set_scratch(False)
+        if cnt<5:
+            cnt=cnt+1
+        if isView(view.id()) and (GetVar('_ISGBKFILE')=='' or view.encoding() != 'UTF-8') \
+            and file_encoding(view)=='GBK':
+            view.run_command('to_utf8')
